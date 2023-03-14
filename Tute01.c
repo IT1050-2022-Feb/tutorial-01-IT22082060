e1fcbc3b57471c11dc39c7c9c9b5160b1b7c18b2
Sanuda Sandipa <127759356+IT22082060@users.noreply.github.com>
2023-03-14 14:19:18 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   inn num1, num2, avg;
+   printf("Enter first subject mark :");
+   scanf("%d", &num1);
+   
+   printf("Enter first subject mark :");
+   scanf("%d", &num2);
+   
+   avg= (num1+num2)/2;
+   
+   printf("Average of the two marks is : %d", avg);
+   
+   
   
   return 0;
 }
